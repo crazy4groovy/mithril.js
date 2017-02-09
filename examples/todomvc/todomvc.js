d37dad908345da8ab716a9da44d9ca054afd453e
solsen <spam2steve@gmail.com>
2017-02-08 21:46:31 -0500

    isolate model from view

--- examples/todomvc/todomvc.js
+++ examples/todomvc/todomvc.js
@@ -9,47 +9,52 @@
   store = (function () {
     var model = {
       todos: JSON.parse(localStorage['todos-mithril'] || '[]'),
+      todosByStatus: [],
       editing: null,
+      editingIdx: -1,
       showing: '',
-      remaining: 0,
-      todosByStatus: []
+      remaining: 0
     }
 
     return {
-      model: {},
+      model: function getModel () { return JSON.parse(JSON.stringify(model)) },
 
       createTodo: function (title) {
-        model.todos.push({ title: title.trim(), completed: false })
+        if (title) model.todos.push({ title: title.trim(), completed: false })
       },
       setStatuses: function (completed) {
         for (var i = 0; i < model.todos.length; i++) model.todos[i].completed = completed
       },
-      setStatus: function (todo, completed) {
-        todo.completed = completed
+      toggleStatus: function (todoIdx) {
+        model.todosByStatus[todoIdx].completed = !model.todosByStatus[todoIdx].completed
       },
-      destroy: function (todo) {
+      destroy: function (todoIdx) {
+        var todo = model.todosByStatus[todoIdx]
         var index = model.todos.indexOf(todo)
         if (index > -1) model.todos.splice(index, 1)
       },
       clear: function () {
-        for (var i = 0; i < model.todos.length; i++) {
-          if (model.todos[i].completed) store.destroy(model.todos[i--])
+        var len = model.todos.length
+        while (len--) {
+          if (model.todos[len].completed) store.destroy(len)
         }
       },
 
-      edit: function (todo) {
-        console.log('EDITING')
+      edit: function (todoIdx) {
+        var todo = model.todosByStatus[todoIdx]
         model.editing = todo
+        model.editingIdx = todoIdx
       },
       update: function (title) {
         if (model.editing != null) {
           model.editing.title = title.trim()
-          if (model.editing.title === '') store.destroy(model.editing)
-          model.editing = null
+          if (model.editing.title === '') store.destroy(model.editingIdx)
+          store.reset()
         }
       },
       reset: function () {
         model.editing = null
+        model.editingIdx = -1
       },
 
       computeModel: function (vnode) {
@@ -62,9 +67,6 @@
             case 'completed': return todo.completed
           }
         })
-
-        // TODO: take a snapshot of the model, for the view
-        store.model = model
       },
 
       dispatch: function (action, args) {
@@ -72,20 +74,13 @@
         nextTick(function () {
           localStorage['todos-mithril'] = JSON.stringify(model.todos)
         })
-      },
-      onDispatch: function (action, args) {
-        return function () {
-          store.dispatch(action, args)
-        }
       }
     }
   })()
 
-  console.log(store)
-
   // view ////////////////////////////////////
 
-  var Todos = {
+  var TodosComponent = {
     add: function (e) {
       if (e.keyCode === 13) {
         store.dispatch(store.createTodo, [e.target.value])
@@ -95,19 +90,24 @@
     toggleAll: function () {
       store.dispatch(store.setStatuses, [document.getElementById('toggle-all').checked])
     },
-    toggle: function (todo) {
-      store.dispatch(store.setStatus, [todo, !todo.completed])
-    },
-    focus: function (vnode, todo) {
-      if (todo === store.model.editing && vnode.dom !== document.activeElement) {
-        vnode.dom.value = todo.title
+    focus: function (vnode, todoIdx, title) {
+      if (
+        todoIdx === store.model().editingIdx &&
+        vnode.dom !== document.activeElement
+      ) {
+        vnode.dom.value = title
         vnode.dom.focus()
-        vnode.dom.selectionStart = vnode.dom.selectionEnd = todo.title.length
+        vnode.dom.selectionStart = vnode.dom.selectionEnd = title.length
       }
     },
     save: function (e) {
       if (e.keyCode === 13 || e.type === 'blur') store.dispatch(store.update, [e.target.value])
       else if (e.keyCode === 27) store.dispatch(store.reset)
+    },
+    onDispatch: function (action, args) {
+      return function () {
+        store.dispatch(action, args)
+      }
     },
 
     oninit: store.computeModel,
@@ -115,39 +115,44 @@
 
     view: function (vnode) {
       var ui = vnode.state
+      var storeModel = store.model()
+
       return [
         $('header.header', [
           $('h1', 'todos'),
           $("input#new-todo[placeholder='What needs to be done?'][autofocus]", { onkeypress: ui.add })
         ]),
-        $('section#main', { style: { display: store.model.todos.length > 0 ? '' : 'none' } }, [
-          $("input#toggle-all[type='checkbox']", { checked: store.model.remaining === 0, onclick: ui.toggleAll }),
+
+        $('section#main', { style: { display: storeModel.todos.length > 0 ? '' : 'none' } }, [
+          $("input#toggle-all[type='checkbox']", { checked: storeModel.remaining === 0, onclick: ui.toggleAll }),
           $("label[for='toggle-all']", { onclick: ui.toggleAll }, 'Mark all as complete'),
           $('ul#todo-list', [
-            store.model.todosByStatus.map(function (todo) {
-              return $('li', { class: (todo.completed ? 'completed' : '') + ' ' + (todo === store.model.editing ? 'editing' : '') }, [
-                $('.view', [
-                  $("input.toggle[type='checkbox']", { checked: todo.completed, onclick: function () { ui.toggle(todo) } }),
-                  $('label', { ondblclick: store.onDispatch(store.edit, [todo]) }, todo.title),
-                  $('button.destroy', { onclick: store.onDispatch(store.destroy, [todo]) })
+            storeModel.todosByStatus.map(function (todo, idx) {
+              return $('li', { class: (todo.completed ? 'completed' : '') + ' ' + (idx === storeModel.editingIdx ? 'editing' : '') }, [
+                $('div.view', [
+                  $("input.toggle[type='checkbox']", { checked: todo.completed, onclick: ui.onDispatch(store.toggleStatus, [idx]) }),
+                  $('label', { ondblclick: ui.onDispatch(store.edit, [idx]) },
+                    todo.title),
+                  $('button.destroy', { onclick: ui.onDispatch(store.destroy, [idx]) })
                 ]),
-                $('input.edit', { onupdate: function (vnode) { ui.focus(vnode, todo) }, onkeypress: ui.save, onblur: ui.save })
+                $('input.edit', { onupdate: function (vnode) { ui.focus(vnode, idx, todo.title) }, onkeypress: ui.save, onblur: ui.save })
               ])
             })
           ])
         ]),
-        store.model.todos.length
+
+        storeModel.todos.length
         ? $('footer#footer', [
           $('span#todo-count', [
-            $('strong', store.model.remaining),
-            store.model.remaining === 1 ? ' item left' : ' items left'
+            $('strong', storeModel.remaining),
+            storeModel.remaining === 1 ? ' item left' : ' items left'
           ]),
           $('ul#filters', [
-            $('li', $("a[href='/']", { oncreate: m.route.link, class: store.model.showing === '' ? 'selected' : '' }, 'All')),
-            $('li', $("a[href='/active']", { oncreate: m.route.link, class: store.model.showing === 'active' ? 'selected' : '' }, 'Active')),
-            $('li', $("a[href='/completed']", { oncreate: m.route.link, class: store.model.showing === 'completed' ? 'selected' : '' }, 'Completed'))
+            $('li', $("a[href='/']", { oncreate: m.route.link, class: storeModel.showing === '' ? 'selected' : '' }, 'All')),
+            $('li', $("a[href='/active']", { oncreate: m.route.link, class: storeModel.showing === 'active' ? 'selected' : '' }, 'Active')),
+            $('li', $("a[href='/completed']", { oncreate: m.route.link, class: storeModel.showing === 'completed' ? 'selected' : '' }, 'Completed'))
           ]),
-          $('button#clear-completed', { onclick: store.onDispatch(store.clear) }, 'Clear completed')
+          $('button#clear-completed', { onclick: ui.onDispatch(store.clear) }, 'Clear completed')
         ])
         : null
       ]
@@ -155,7 +160,7 @@
   }
 
   m.route(document.getElementById('todoapp'), '/', {
-    '/': Todos,
-    '/:status': Todos
+    '/': TodosComponent,
+    '/:status': TodosComponent
   })
 })()
